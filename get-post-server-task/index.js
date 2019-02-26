0391d3c1d6f0e5dc44f2536a717c610d3fd53f8c
n.andreychenko <natandreychenko@gmail.com>
2019-02-27 00:55:53 +0300

    Add get request handler

--- get-post-server-task/index.js
+++ get-post-server-task/index.js
@@ -1,27 +1,8 @@
-/*
- ЗАДАЧА
- Написать HTTP-сервер для загрузки и получения файлов.
- - Все файлы находятся в директории files.
- - Структура файлов НЕ вложенная.
- - Виды запросов к серверу:
-   GET /file.ext
-   - выдаёт файл file.ext из директории files;
-   - правильный mime-type по содержимому (модуль mime).
-   POST /file.ext
-   - пишет всё тело запроса в файл files/file.ext и выдаёт ОК;
-   - если файл уже есть, то выдаёт ошибку 409;
-   - при превышении файлом размера 1MB выдаёт ошибку 413;
-   - так как поддиректорий нет, то при наличии / или .. в пути сервер должен выдавать ошибку 400;
-   - вместо file может быть любое имя файла.
- - Сервер должен корректно обрабатывать ошибки "файл не найден" и другие (ошибка чтения файла).
- - index.html или curl для тестирования.
-*/
-
-// Пример простого сервера в качестве основы
-
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const path = require('path');
+const mime = require('mime/lite');
 
 http.createServer((req, res) => {
   const pathname = decodeURI(url.parse(req.url).pathname);
@@ -29,17 +10,71 @@ http.createServer((req, res) => {
   switch(req.method) {
     case 'GET':
       if (pathname === '/') {
-        // отдачу файлов надо сделать «правильно»
         fs.readFile(`${__dirname}/public/index.html`, (err, content) => {
-          if (err) throw err;
-          res.setHeader('Content-Type', 'text/html;charset=utf-8');
+          if (err) {
+            handleError(err);
+          }
+
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
           res.end(content);
         });
+
         return;
+      } else if (pathname === '/styles.css') {
+        fs.readFile(`${__dirname}/public/styles.css`, (err, content) => {
+          if (err) {
+            handleError(err);
+          }
+
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/css');
+          res.end(content);
+        });
+
+        return;
+      } else if (pathname !== '/favicon.ico'){
+        const filePath = path.normalize(path.join(`${__dirname}/files`, pathname));
+
+        fs.stat(filePath, (err, stats) => {
+          if (err || !stats.isFile()) {
+            handleError(err, res);
+          }
+
+          sendFile(filePath, res);
+        });
       }
+      break;
 
     default:
       res.statusCode = 502;
       res.end('Not implemented');
   }
 }).listen(3000);
+
+function handleError(err, res) {
+  if (err.code === 'ENOENT') {
+    res.statusCode = 404;
+    res.end('Not found');
+    return;
+  }
+
+  res.statusCode = 500;
+  res.end('Internal error');
+}
+
+function sendFile(file, res) {
+  const fileStream = fs.createReadStream(file);
+
+  const mimeType = mime.getType(file);
+  res.setHeader('Content-Type', `${mimeType}; charset=utf-8;`);
+
+  fileStream
+    .on('error', err => {
+      handleError(err, res);
+    })
+    .pipe(res)
+    .on('close', () => {
+      fileStream.destroy();
+    });
+}
